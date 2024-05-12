a647f01b7ba4ed294e26df51f3fd48af2b7e100f
Ammar Ahmed <ammar69aa59@gmail.com>
2024-05-12 14:46:37 -0700

    word-game

---
+++ app.js
@@ -0,0 +1,31 @@
+let display = document.getElementById("h2");
+function alphabets(a_word, b_word, c_word, d_word, e_word, f_word, g_word, h_word, i_word, j_word, k_word, l_word, m_word, n_word, o_word, p_word, q_word, r_word, s_word, t_word, u_word, v_word, w_word, x_word, y_word, z_word) {
+    let random = Math.floor(Math.random() * 4);
+    display.innerText = a_word[random];
+    display.innerText = b_word[random];
+    display.innerText = c_word[random];
+    display.innerText = d_word[random];
+    display.innerText = e_word[random];
+    display.innerText = f_word[random];
+    display.innerText = g_word[random];
+    display.innerText = h_word[random];
+    display.innerText = i_word[random];
+    display.innerText = j_word[random];
+    display.innerText = k_word[random];
+    display.innerText = l_word[random];
+    display.innerText = m_word[random];
+    display.innerText = n_word[random];
+    display.innerText = o_word[random];
+    display.innerText = p_word[random];
+    display.innerText = q_word[random];
+    display.innerText = r_word[random];
+    display.innerText = s_word[random];
+    display.innerText = t_word[random];
+    display.innerText = u_word[random];
+    display.innerText = v_word[random];
+    display.innerText = w_word[random];
+    display.innerText = x_word[random];
+    display.innerText = y_word[random];
+    display.innerText = z_word[random];
+
+};
